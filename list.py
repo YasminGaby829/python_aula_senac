730f3cd9f13737feb6edc6ed5ee2646aef387e94
Yasmin Gabrielle <yasmin.gaby829@gmail.com>
2023-11-30 12:05:22 -0300

    first

---
+++ list.py
@@ -0,0 +1,12 @@
+nomes= ["jose", "maria","pedro","paulo","ana"]
+"""print(nomes)
+nomes.pop()
+print(nomes)
+nomes.append("michel")
+print(nomes)"""
+print(nomes[1])
+print(nomes)
+
+for x in nomes:
+    print(x)
+    
